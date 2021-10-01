f215a818071a9dde4d1448969f7dbe5d754b818c
jlaurentez <72422330@continental.edu.pe>
2021-10-01 17:51:07 -0500

    ButtomNav v1

--- app/src/main/java/com/example/appelectroconsum/ComparativaActivity.kt
+++ app/src/main/java/com/example/appelectroconsum/ComparativaActivity.kt
@@ -2,17 +2,10 @@ package com.example.appelectroconsum
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class HomeActivity : AppCompatActivity() {
-
+class ComparativaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
-
+        setContentView(R.layout.activity_comparativa)
     }
-
-
-
-
 }
