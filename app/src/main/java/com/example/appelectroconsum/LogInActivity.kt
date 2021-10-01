ff5f768aca6628f9491df0b8967aaff8b2347c35
jlaurentez <72422330@continental.edu.pe>
2021-10-01 18:56:14 -0500

    Home y Login Refinado

--- app/src/main/java/com/example/appelectroconsum/LogInActivity.kt
+++ app/src/main/java/com/example/appelectroconsum/LogInActivity.kt
@@ -1,16 +1,18 @@
 package com.example.appelectroconsum
 
 import android.content.Intent
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
 
 class LogInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log_in)
 
-        val btnLogin = findViewById<Button>(R.id.btnLogIn)
+        val btnLogin = findViewById<ImageButton>(R.id.btnLogIn)
         btnLogin.setOnClickListener{
             val lanzar = Intent (this, HomeActivity::class.java)
             startActivity(lanzar)
